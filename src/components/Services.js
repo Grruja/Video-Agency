d1fd6a3ab33d2a0e985eea06b1afc9c622367dab
AleexxG <alexanderg.sln@gmail.com>
2023-03-19 22:55:13 +0100

    Home Page Responsive

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,30 +9,39 @@ function Services() {
         <div className='services_content text-white m-auto position-absolute'>
 
             <div className='text-center w-50 m-auto'>
-                <h3>Services</h3>
+                <h3 className='color fw-bold mb-5'>Services</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
 
-            <div className='d-flex gap-5 mt-5'>
+            <div className='card_container d-flex'>
 
-                <div className='card text-bg-dark p-5'>
-                    <img src={IconOne} alt='img' className='card-img-top w-75 m-auto'></img>
-                    <div className='card-body'>
-                        <p className='card-text text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
+                <div className='card_custom card p-5 border-0'>
+                    <div className='h-100 d-flex justify-content-center align-items-center'>
+                        <img src={IconOne} alt='img' className='card-img-top w-25'></img>
+                    </div>
+                    <div className='card-body mt-4'>
+                        <h3 className='color card-title text-center mb-4'>Voice over</h3>
+                        <p className='card-text text-center fw-lighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                     </div>
                 </div>
 
-                <div className='card text-bg-dark p-5'>
-                    <img src={IconTwo} alt='img' className='card-img-top w-75 m-auto'></img>
-                    <div className='card-body'>
-                        <p className='card-text text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
+                <div className='card_custom card p-5 border-0 bg-dark rounded-0'>
+                    <div className='h-100 d-flex justify-content-center align-items-center'>
+                        <img src={IconTwo} alt='img' className='card-img-top w-25'></img>
+                    </div>
+                    <div className='card-body mt-4'>
+                        <h3 className='color card-title text-center mb-4'>Content voice over</h3>
+                        <p className='card-text text-center fw-lighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                     </div>
                 </div>
 
-                <div className='card text-bg-dark p-5'>
-                    <img src={IconTree} alt='img' className='card-img-top w-75 m-auto'></img>
-                    <div className='card-body'>
-                        <p className='card-text text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
+                <div className='card_custom card p-5 border-0'>
+                    <div className='h-100 d-flex justify-content-center align-items-center'>
+                        <img src={IconTree} alt='img' className='card-img-top w-25'></img>
+                    </div>
+                    <div className='card-body mt-4'>
+                        <h3 className='color card-title text-center mb-4'>Audiobook</h3>
+                        <p className='card-text text-center fw-lighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                     </div>
                 </div>
 
