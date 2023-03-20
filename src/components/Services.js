908b79e51cf907433a333ca71d6576ee680c1dad
AleexxG <alexanderg.sln@gmail.com>
2023-03-20 23:58:42 +0100

    Service Page Responsive

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,7 +8,7 @@ function Services() {
     <div className='services_page position-relative w-100'>
         <div className='services_content text-white m-auto position-absolute'>
 
-            <div className='text-center w-50 m-auto'>
+            <div className='text_box text-center w-50 m-auto'>
                 <h3 className='color fw-bold mb-5'>Services</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
@@ -16,19 +16,21 @@ function Services() {
             <div className='card_container d-flex'>
 
                 <div className='card_custom card p-5 border-0'>
-                    <div className='h-100 d-flex justify-content-center align-items-center'>
+                    <div className='img_box h-100 d-flex justify-content-center align-items-center'>
                         <img src={IconOne} alt='img' className='card-img-top w-25'></img>
                     </div>
+                    <div class="line_custom"></div>
                     <div className='card-body mt-4'>
                         <h3 className='color card-title text-center mb-4'>Voice over</h3>
                         <p className='card-text text-center fw-lighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                     </div>
                 </div>
 
-                <div className='card_custom card p-5 border-0 bg-dark rounded-0'>
-                    <div className='h-100 d-flex justify-content-center align-items-center'>
+                <div className='card p-5 border-0 bg-dark rounded-0'>
+                    <div className='img_box h-100 d-flex justify-content-center align-items-center'>
                         <img src={IconTwo} alt='img' className='card-img-top w-25'></img>
                     </div>
+                    <div class="line_custom"></div>
                     <div className='card-body mt-4'>
                         <h3 className='color card-title text-center mb-4'>Content voice over</h3>
                         <p className='card-text text-center fw-lighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
@@ -36,9 +38,10 @@ function Services() {
                 </div>
 
                 <div className='card_custom card p-5 border-0'>
-                    <div className='h-100 d-flex justify-content-center align-items-center'>
+                    <div className='img_box h-100 d-flex justify-content-center align-items-center'>
                         <img src={IconTree} alt='img' className='card-img-top w-25'></img>
                     </div>
+                    <div class="line_custom"></div>
                     <div className='card-body mt-4'>
                         <h3 className='color card-title text-center mb-4'>Audiobook</h3>
                         <p className='card-text text-center fw-lighter'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
